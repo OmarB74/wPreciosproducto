cedf9b5c014f73ac42889320ec7d7c9f7b17af90
Enrique Omar Bolaños <eomarbs74@gmail.com>
2018-10-15 13:05:01 -0500

    primer commit

---
+++ wPreciosproducto/FrmPrecioProducto.Designer.vb
@@ -0,0 +1,118 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmPrecioProducto
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnBuscar = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.lbCodigoBarras = New System.Windows.Forms.Label()
+        Me.txtCodigoBarras = New System.Windows.Forms.TextBox()
+        Me.lPrecio = New System.Windows.Forms.Label()
+        Me.txtPrecio = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnBuscar
+        '
+        Me.btnBuscar.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBuscar.Location = New System.Drawing.Point(416, 50)
+        Me.btnBuscar.Name = "btnBuscar"
+        Me.btnBuscar.Size = New System.Drawing.Size(93, 41)
+        Me.btnBuscar.TabIndex = 0
+        Me.btnBuscar.Text = "&Buscar"
+        Me.btnBuscar.UseVisualStyleBackColor = True
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSalir.Location = New System.Drawing.Point(416, 111)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(93, 41)
+        Me.btnSalir.TabIndex = 1
+        Me.btnSalir.Text = "&Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'lbCodigoBarras
+        '
+        Me.lbCodigoBarras.AutoSize = True
+        Me.lbCodigoBarras.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbCodigoBarras.Location = New System.Drawing.Point(43, 50)
+        Me.lbCodigoBarras.Name = "lbCodigoBarras"
+        Me.lbCodigoBarras.Size = New System.Drawing.Size(156, 24)
+        Me.lbCodigoBarras.TabIndex = 2
+        Me.lbCodigoBarras.Text = "Codigo de Barras"
+        '
+        'txtCodigoBarras
+        '
+        Me.txtCodigoBarras.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtCodigoBarras.Location = New System.Drawing.Point(205, 55)
+        Me.txtCodigoBarras.MaxLength = 24
+        Me.txtCodigoBarras.Name = "txtCodigoBarras"
+        Me.txtCodigoBarras.Size = New System.Drawing.Size(205, 29)
+        Me.txtCodigoBarras.TabIndex = 3
+        '
+        'lPrecio
+        '
+        Me.lPrecio.AutoSize = True
+        Me.lPrecio.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lPrecio.Location = New System.Drawing.Point(52, 100)
+        Me.lPrecio.Name = "lPrecio"
+        Me.lPrecio.Size = New System.Drawing.Size(74, 24)
+        Me.lPrecio.TabIndex = 4
+        Me.lPrecio.Text = "Precio :"
+        '
+        'txtPrecio
+        '
+        Me.txtPrecio.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPrecio.Location = New System.Drawing.Point(205, 97)
+        Me.txtPrecio.MaxLength = 12
+        Me.txtPrecio.Name = "txtPrecio"
+        Me.txtPrecio.Size = New System.Drawing.Size(106, 29)
+        Me.txtPrecio.TabIndex = 5
+        '
+        'FrmPrecioProducto
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(601, 198)
+        Me.Controls.Add(Me.txtPrecio)
+        Me.Controls.Add(Me.lPrecio)
+        Me.Controls.Add(Me.txtCodigoBarras)
+        Me.Controls.Add(Me.lbCodigoBarras)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnBuscar)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "FrmPrecioProducto"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Precio Producto"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnBuscar As Button
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents lbCodigoBarras As Label
+    Friend WithEvents txtCodigoBarras As TextBox
+    Friend WithEvents lPrecio As Label
+    Friend WithEvents txtPrecio As TextBox
+End Class
---
+++ wPreciosproducto/FrmPrecioProducto.vb
@@ -0,0 +1,18 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class FrmPrecioProducto
+    Private Sub btnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        Close()
+    End Sub
+
+    Private Sub btnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        Dim sCadena As String = "server=localhost;database=precios;user id=root;password=ABC123abc;port=3306;"
+        Dim conn As New MySqlConnection(sCadena)
+        Try
+            conn.Open()
+        Catch ex As Exception
+            MsgBox("Error")
+        End Try
+        conn.Close()
+
+    End Sub
+End Class
